a6de99b792af1f8d71127cd32747c623a9953915
13rstuart7 <135680232+13rstuart7@users.noreply.github.com>
2023-08-26 17:12:02 -0600

    finished

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
 // Assignment code here
-
+var characterbase=""
+function generatePassword(){
+  if(confirm("would you like to include lower case characters?")){
+    characterbase+="abcdefghijklmnopqrstuvwxyz"
+  }
+  if(confirm("would you like to upper case characters?")){
+    characterbase+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if(confirm("would you like to include numbers?")){
+    characterbase+="1234567890"
+  }
+  if(confirm("would you like to include special characters?")){
+    characterbase+="!@#$%^&*()"
+  }
+  if(characterbase<1){
+    return
+  }
+  var passwordlength=prompt("please enter length between 8-128")
+  if (passwordlength<8||passwordlength>129){
+    return
+  }
+  var password=""
+  for(i=0;i<passwordlength;i++){
+    password+=characterbase.charAt(Math.floor(Math.random()*characterbase.length))
+  }
+  return password
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
